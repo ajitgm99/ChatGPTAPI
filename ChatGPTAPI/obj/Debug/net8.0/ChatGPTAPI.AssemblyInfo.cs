eba4ad872b0da002d536417be42ce32431933938
Ajit Mhatre <ajitgm99@gmail.com>
2024-07-03 07:22:26 +0530

    checkin propush Project

--- ChatGPTAPI/obj/Debug/net8.0/ChatGPTAPI.AssemblyInfo.cs
+++ ChatGPTAPI/obj/Debug/net8.0/ChatGPTAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatGPTAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d855c126132225ddcc3df603a1f169acdd4a06e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatGPTAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatGPTAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
